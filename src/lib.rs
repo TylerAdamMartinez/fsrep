a4a848e7fff22e0669dde3f2f74859339b8b263d
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-03-12 21:23:01 -0600

    test(lib.rs): add search_found test

--- src/lib.rs
+++ src/lib.rs
@@ -28,11 +28,47 @@ impl Config<'_> {
 
 pub fn run_process(program_config: &Config) -> Result<(), Box<dyn Error>> {
     let file_contents: String = fs::read_to_string(&program_config.filename)?;
-    println!("File contents: \n{}", &file_contents);
+    let regex_query_results: Vec<&str> = search(&program_config.regex_query, &file_contents);
+    print_results(&regex_query_results);
     Ok(())
+}
+
+fn search<'a>(regex_query: &str, file_contents: &'a str) -> Vec<&'a str> {
+    let mut regex_query_results = Vec::<&str>::new();
+
+    for line in file_contents.lines() {
+        if line.contains(regex_query) {
+            regex_query_results.push(line);
+        }
+    }
+
+    regex_query_results
+}
+
+fn print_results(regex_query_results: &Vec<&str>) {
+    for result in regex_query_results.iter() {
+        println!("{}", result);
+    }
 }
 
 pub fn fsrep_failure(error_flag: impl Display) {
         println!("fsrep failure: {}", error_flag);
         process::exit(1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_founded() {
+        let query: &str = "Lorem";
+        let contents: &str = "\
+            Lorem
+            search
+            and
+            destroy";
+
+        assert_eq!(vec!["Lorem"], search(&query, &contents));
+    }
 }
